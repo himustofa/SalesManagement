331e3ebb3e8efb9577fea188900938615c06dcdb
himus <mustofa2008@gmail.com>
2018-08-11 02:52:15 +0600

    Add welcome screen in MainActivity Add HomeActivity Add Navigation in HomeActivity

---
+++ app/src/main/java/com/sm/demo/salesmanagement/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.sm.demo.salesmanagement;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_id);
+        toggle = new ActionBarDrawerToggle(HomeActivity.this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
